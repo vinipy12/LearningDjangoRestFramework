66605e4783d2b6e06f78894be0e2b18dcd2ef9da
vinipy12 <vinicius.loureiro.cavalcante12@gmail.com>
2025-05-27 20:57:51 -0400

    Creating Comment model.

--- blog/models.py
+++ blog/models.py
@@ -48,3 +48,10 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
