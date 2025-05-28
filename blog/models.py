169ef39b9a39d942947056951442fb9fe8c33bfa
vinipy12 <vinicius.loureiro.cavalcante12@gmail.com>
2025-05-27 20:55:23 -0400

    Creating Post model.

--- blog/models.py
+++ blog/models.py
@@ -40,3 +40,11 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, blank=False, null=False)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
